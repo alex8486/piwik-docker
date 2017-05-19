54e4a66d172d01307ee651e3289388b4b68c420e
Alexander Jacobi <alexander.jacobi@t-systems.com>
2017-05-19 23:06:17 +0200

    try to fix error in sed command

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ export LD_PRELOAD=libnss_wrapper.so
 export NSS_WRAPPER_PASSWD=/tmp/passwd
 export NSS_WRAPPER_GROUP=/etc/group
 
-sed -i "s/ALLOWED_HOSTNAME/${ALLOWED_HOSTNAME}/g" /etc/nginx/conf.d/default.conf
+sed -i 's/ALLOWED_HOSTNAME/'${ALLOWED_HOSTNAME}'/g' /etc/nginx/conf.d/default.conf
 
 if [ ! -f /var/www/piwik/index.php ]; then
   # Copy initial site
