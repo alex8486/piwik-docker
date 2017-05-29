f5cbdc28b8b15df1919e53c5f53bd03f3a7dc5dc
Alexander Jacobi <alexander.jacobi@t-systems.com>
2017-05-29 12:02:56 +0200

    added vi to apt-get install and added ability to copy config.ini.php

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,20 @@ export LD_PRELOAD=libnss_wrapper.so
 export NSS_WRAPPER_PASSWD=/tmp/passwd
 export NSS_WRAPPER_GROUP=/etc/group
 
+
+if [ $IS_ALREADY_INSTALLED == TRUE ]; then
+
+  mkdir -p /var/www/piwik/config/
+  cp /workdir/config/config.ini.php /var/www/piwik/config/
+
+  sed -i "s/DATABASE_HOST/${DATABASE_HOST}/g" /var/www/piwik/config/config.ini.php
+  sed -i "s/DATABASE_USERNAME/${DATABASE_USERNAME}/g" /var/www/piwik/config/config.ini.php
+  sed -i "s/DATABASE_PASSWORD/${DATABASE_PASSWORD}/g" /var/www/piwik/config/config.ini.php
+  sed -i "s/DATABASE_NAME/${DATABASE_NAME}/g" /var/www/piwik/config/config.ini.php
+  sed -i "s/ALLOWED_HOSTNAME/${ALLOWED_HOSTNAME}/g" /var/www/piwik/config/config.ini.php
+
+fi
+
 sed -i "s/ALLOWED_HOSTNAME/${ALLOWED_HOSTNAME}/g" /etc/nginx/conf.d/default.conf
 
 if [ ! -f /var/www/piwik/index.php ]; then
